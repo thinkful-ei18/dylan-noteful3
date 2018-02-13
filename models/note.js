7715c6cb585ed7095e68c65d22629215aada5c7e
Dylan <nielsen.dylan@gmail.com>
2018-02-13 11:15:42 -0800

    Write database seed and note model

---
+++ models/note.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema({
+  title: String,
+  content: String,
+  create: {type: Date, default: Date.now}
+});
+
+const Note = mongoose.model('Note', noteSchema);
---
+++ utils/seed-database.js
@@ -0,0 +1,32 @@
+'use strict';
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { MONGODB_URI } = require('./config');
+const Note = require('../models/note');
+
+const seedNotes = require('../db/seed/notes');
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    return mongoose.connection.db.dropDatabase()
+      .then(result => {
+        console.info('Dropped database:', result);
+      });
+  })
+  .then(() => {
+    return Note.insertMany(seedNotes)
+      .then(results => {
+        console.info('Inserted Notes:', results.length);
+      });
+  })
+  .then(() => {
+    return mongoose.disconnect()
+      .then(() => {
+        console.info('Disconnected');
+      });
+  })
+  .catch(err => {
+    console.error('ERROR:', err.message);
+    console.error(err);
+  });
