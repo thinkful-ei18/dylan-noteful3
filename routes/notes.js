c912b3cc792ca748239f2e0728c9234a07ab3d4d
Dylan <nielsen.dylan@gmail.com>
2018-02-13 13:29:28 -0800

    Conditionalized GET /notes

--- routes/notes.js
+++ routes/notes.js
@@ -9,17 +9,27 @@ const mongoose = require('mongoose');
 /* ========== GET/READ ALL ITEM ========== */
 router.get('/notes', (req, res, next) => {
   const { searchTerm } = req.query;
-  Note.find(
-    { $text: { $search: searchTerm } },
-    { score: { $meta: 'textScore' } }
-  )
-    .sort({ score: { $meta: 'textScore' } })
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      next(err);
-    });
+  if(searchTerm) {
+    Note.find(
+      { $text: { $search: searchTerm } },
+      { score: { $meta: 'textScore' } }
+    )
+      .sort({ score: { $meta: 'textScore' } })
+      .then(response => {
+        res.json(response);
+      })
+      .catch(err => {
+        next(err);
+      });
+  } else {
+    Note.find()
+      .then(response => {
+        res.json(response);
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
@@ -39,7 +49,6 @@ router.get('/notes/:id', (req, res, next) => {
       }
     })
     .catch(err => {
-      console.log(err);
       next(err);
     });
 });
@@ -62,7 +71,6 @@ router.post('/notes', (req, res, next) => {
 
   Note.create(newItem)
     .then(response => {
-      console.log(response);
       if (response) {
         res
           .location(`${req.originalUrl}/${response.id}`)
