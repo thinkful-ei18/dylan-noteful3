0e3a755958b418095239de16d71ffc8fa0220839
Dylan <nielsen.dylan@gmail.com>
2018-02-14 11:43:39 -0800

    Set up database seeding for notes

--- test/notes.test.js
+++ test/notes.test.js
@@ -1,3 +1,5 @@
+import { mongo } from 'mongoose';
+
 'use strict';
 const app = require('../server');
 const chai = require('chai');
@@ -12,3 +14,20 @@ const mongoose = require('mongoose');
 const {TEST_MONGODB_URI} = require('../config');
 const {Note} = require('../models/note');
 const seedData = require('../db/seed/notes');
+
+before(function() {
+  return mongoose.connect(TEST_MONGODB_URI, { autoIndex: false });
+});
+
+beforeEach(function() {
+  return Note.insertMany(seedData)
+    .then(() => Note.ensureIndexes());
+});
+
+afterEach(function() {
+  return mongoose.connection.db.dropDatabase();
+});
+
+after(function() {
+  return mongoose.disconnect();
+});
