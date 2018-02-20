28b8ce342cdde08120863f8c70400355b47488f9
Dylan <nielsen.dylan@gmail.com>
2018-02-20 11:17:31 -0800

    Create user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.set('toObject', {
+  transform: function(doc, ret) {
+    delete ret.password;
+    ret.id = ret._id;
+    delete ret.__v;
+    delete ret._id;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
